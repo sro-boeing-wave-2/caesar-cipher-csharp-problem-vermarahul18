40342380dd2b431b0595270d08461490439deed9
Rahul <vermarahulkps@gmail.com>
2018-07-19 10:24:38 +0530

    first commit

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,6 +7,38 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
+            string newstr = "";
+            int x = 0;
+            for (int i = 0; i < text.Length; i++) {
+                if (shiftKey == 26)
+                {
+                    return text;
+                }
+                else if ((int)text[i] >= 65 && (int)text[i] <= 90)
+                {
+                    x = (int)text[i] + shiftKey;
+                    if (x > 90)
+                    {
+                        newstr += Convert.ToChar(x - 26);
+                    }
+                    else { newstr += Convert.ToChar(x); }
+                }
+                else if ((int)text[i] >= 97 && (int)text[i] <= 122)
+                {
+                    x = (int)text[i] + shiftKey;
+                    if (x > 122)
+                    {
+                        newstr += Convert.ToChar(x - 26);
+                    }
+                    else { newstr += Convert.ToChar(x); }
+                }
+                else {
+                    newstr += text[i];
+                }
+            }
+
+            return newstr;
+
             throw new NotImplementedException("You need to implement this function.");
         }
     }
